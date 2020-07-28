76c271c660e1a70d097657e8dfddd74088c5f5e6
Ashna Agrawal <51378366+ashnaagrawal@users.noreply.github.com>
2020-07-28 21:11:15 +0530

    Add files via upload

--- hotel mangement/frontend.py
+++ hotel mangement/frontend.py
@@ -28,9 +28,63 @@ f4.pack(side=LEFT)
 #=========================MAIN SCREEN===========================
 txt_impute = StringVar(value='Master Python Today...')
 Display = Entry(Tops,font=('arial',97,'bold'),fg='white',bd=50,bg='blue',justify='right',textvariable=txt_impute)
-Display.grid(columnspan=3)
+Display.grid(columnspan=4)
+
+#============================Date and Time=========================
+localtime = time.asctime(time.localtime(time.time()))
+
+lblinfo = Label(f2,font=('arial',20,'bold'),text=localtime,fg='dark blue',bd=10,anchor=W)
+lblinfo.grid(row=0,column=0,columnspan=4)
+
+#========================Row 1=======================
+
+btn7 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='7')
+btn7.grid(row=1,column=0)
+btn8 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='8')
+btn8.grid(row=1,column=1)
+btn9 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='9')
+btn9.grid(row=1,column=2)
+btnC = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='C')
+btnC.grid(row=1,column=3)
+
+#========================Row 2=======================
+
+btn4 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='4')
+btn4.grid(row=2,column=0)
+btn5 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='5')
+btn5.grid(row=2,column=1)
+btn6 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='6')
+btn6.grid(row=2,column=2)
+btnplus = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='+')
+btnplus.grid(row=2,column=3)
 
 
 
+#========================Row 3=======================
+
+btn1 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='1')
+btn1.grid(row=3,column=0)
+btn2 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='2')
+btn2.grid(row=3,column=1)
+btn3 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='3')
+btn3.grid(row=3,column=2)
+btnminus = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='-')
+btnminus.grid(row=3,column=3)
+
+
+#========================Row 4=======================
+btn1 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='1')
+btn1.grid(row=3,column=0)
+btn2 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='2')
+btn2.grid(row=3,column=1)
+btn3 = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='3')
+btn3.grid(row=3,column=2)
+btnminus = Button(f2,padx=15,pady=5,border=8,font=('arial',30,'bold'),text='-')
+btnminus.grid(row=3,column=3)
+
+
+
+
+#========================Row 5=======================
 
 root.mainloop()
